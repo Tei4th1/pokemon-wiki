63bd1a8e84fc5600480d905b35f2d63ffbbf12f9
Zhdan <zhdanp95@mail.ru>
2025-03-03 18:05:08 +0300

    main page gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client';
 
+import { Background } from '@/components/background';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Background bgcolor="linear-gradient(to bottom, #f5db13, #f2b807)"></Background>
       <Footer />
     </>
   );
